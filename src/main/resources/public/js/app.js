3a056041b19f3ad312d24950b6623cae93cf3aa2
Lazar <sharipol@oregonstate.edu>
2017-02-15 17:05:59 -0800

    Added overlay menu and put player board and place ship functions onto overlay.

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -8,6 +8,13 @@ $( document ).ready(function() {
    });
 });
 
+
+            function openNav() {
+                document.getElementById("myNav").style.height = "100%";
+            }
+            function closeNav() {
+                document.getElementById("myNav").style.height = "0%";
+            }
 function placeShip() {
    console.log($( "#shipSelec" ).val());
    console.log($( "#rowSelec" ).val());
